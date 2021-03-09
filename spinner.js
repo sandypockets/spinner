da71373018b7c318a227edce7b7f2de7400296f1
Zac Alexander <zac@znacreative.com>
2021-03-09 19:48:47 +0000

    A loading spinner for the CLI

---
+++ spinner.js
@@ -0,0 +1,30 @@
+const spinner = () => {
+  let delay = 100;
+  let counter = 0;
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, delay);
+    delay += 200;
+  
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, delay);
+  delay += 200;
+  
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, delay);
+  delay += 200;
+  
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   '); 
+  }, delay);
+  delay += 200;
+
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, delay);
+};
+
+spinner();
